155c272012fbc673b8460127ef37283a4ee2a31a
Chepkosgei-Anastansia <anastansiachepkosgei@gmail.com>
2023-07-25 09:11:33 +0300

    Modification of the app's database

--- asset_management_project/asset/views.py
+++ asset_management_project/asset/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
-# Create your views here.
-def storeHome(request):
+from .models import Asset
+from django.views.generic.list import ListView 
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-    return render(request, "store/storeSummary.html")
 
+class AssetList(ListView):
+    model = Asset
+    template_name = "asset/instore.html"
+    context_object_name = 'assets'
+
+class ProjectCreate(CreateView):
+    model = Asset
+    fields = ['title','description','lead_developer_id', 'owner']
+    success_url = reverse_lazy('projects')  
+    template_name = 'base/add_project.html'
+    form_class = AssetCreation
--- asset_management_project/asset_management_project/settings.py
+++ asset_management_project/asset_management_project/settings.py
@@ -80,10 +80,22 @@ WSGI_APPLICATION = "asset_management_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'AssetsDB',
+        'USER': 'root',
+        'PASSWORD': 'Herod@7915',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
